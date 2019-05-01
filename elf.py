2409fba676b535dd6052e2de1adf467d0f1b5949
Carey Hoffman <cgchoffman@gmail.com>
2019-04-30 17:11:41 -0700

    Add new background.  Always .convert() images when you load them...

--- elf.py
+++ elf.py
@@ -6,10 +6,15 @@ import pygame
 import gameObjects
 import random
 import os
+import tkinter
 
 # Window Options
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 400
+root = tkinter.Tk()
+# not sure if these calls are high DPI friendly.
+OS_SCREEN_WIDTH = root.winfo_screenwidth()
+OS_SCREEN_HEIGHT = root.winfo_screenheight()
+# SCREEN_WIDTH =
+# SCREEN_HEIGHT =
 SCREEN_TITLE = "CROSSY RPG"
 
 # Color vars, RGB
@@ -20,7 +25,7 @@ BLACK = (0, 0, 0)
 # Clock used to update game events and frames
 clock = pygame.time.Clock()
 pygame.font.init()
-font = pygame.font.SysFont("comicsans", 30)
+font = pygame.font.SysFont("courier", 30)
 
 
 class Game:
@@ -29,20 +34,33 @@ class Game:
     is_game_over = False
     did_win = False
 
-    def __init__(self, background_img_path, title, width, height):
-        #  This set_mode must happen before anything graphical happens
-        self.game_screen = pygame.display.set_mode(
-            (SCREEN_WIDTH, SCREEN_HEIGHT))
+    def __init__(self, background_img_path, title):
+        self.background_img_path = background_img_path
+        self.load_screen()
+        self.load_background()
         self.theme_song = pygame.mixer.Sound(os.path.join(
             'sounds', 'Hypnotic NES.wav'))
         self.title = title
-        self.width = width
         # Screen game screen
         self.player = gameObjects.PlayerCharacter(
-            200, 200)
+            839, 215)
 
         # Set the title of the screen
         pygame.display.set_caption(self.title)
+
+    def load_screen(self, ):
+        # This set_mode must happen before anything graphical happens
+        self.game_screen = pygame.display.set_mode((0, 0),
+                                                   pygame.FULLSCREEN | pygame.RESIZABLE | pygame.NOFRAME)
+
+    def load_background(self,):
+        self.background_img = pygame.image.load(
+            os.path.join("imgs", "fantasy-2048-x-1536_full.png")).convert_alpha()
+        self.background_rect = self.background_img.get_rect()
+
+    def draw_background(self, ):
+        self.game_screen.fill(WHITE)
+        self.game_screen.blit(self.background_img, (0, 0))
 
     def show_msg(self, txt, duration=1):
         text = font.render(txt, False, RED, BLACK)
@@ -50,22 +68,13 @@ class Game:
         pygame.display.update()
         clock.tick(duration)
 
-    def generate_enemies(self, round):
-        i = 0
-        self.enemies = []
-        while i < round:
-            enemy = gameObjects.EnemyCharacter(random.randint(0, self.width),
-                                               random.randint(0, self.height))
-            enemy.SPEED *= (self.round/2)
-            self.enemies.append(enemy)
-            i += 1
-
     def draw_screen(self):
         # print("drawing game")
         # Clear screen
-        self.game_screen.fill(WHITE)
+        self.draw_background()
         # Draw their new pos
         self.player.draw(self.game_screen)
+        pygame.display.flip()
 
     def run_game_loop(self):
         direction = 0
@@ -79,18 +88,18 @@ class Game:
             # Create the event loop
             keys = pygame.key.get_pressed()  # checking pressed keys
             if keys[pygame.K_UP]:
-                print("up")
                 # don't jump again if it's already jumping
                 if not self.player.jumping == True:
                     self.player.jump()
             if keys[pygame.K_DOWN]:
-                print("down")
+                pass  # squat()?
             if keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]:
-                print("left and right")
                 self.player.walk()
             for event in pygame.event.get():
-                # if event.type == pygame.KEYDOWN:
-                #     if event.key == pygame.K_UP:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
+                        self.is_game_over = True
+                    # if event.key == pygame.K_UP:
                 #         print("up")
                 #         # don't jump again if it's already jumping
                 #         if not self.player.jumping == True:
@@ -129,8 +138,8 @@ class Game:
 
 pygame.init()
 
-new_game = Game(os.path.join("imgs", "background.png"), SCREEN_TITLE,
-                SCREEN_HEIGHT, SCREEN_WIDTH)
+new_game = Game(os.path.join(
+    "imgs", "fantasy-2048-x-1536_full.png"), SCREEN_TITLE)
 new_game.run_game_loop()
 
 
